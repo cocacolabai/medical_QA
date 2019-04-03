352d6a8d43165534b519acbb5fe87642b58e17d9
liuchenxu@qq <liuchenxu@bupt.edu.cn>
2019-04-03 22:43:59 +0800

    update

--- get_data/build_data.py
+++ get_data/build_data.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
-
-
-
-from lxml import etree
+import json
 import os
 from max_cut import *
 
 class MedicalGraph:
     def __init__(self):
-        self.conn = pymongo.MongoClient()
-        cur_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
-        self.db = self.conn['medical']
-        self.col = self.db['data']
-        first_words = [i.strip() for i in open(os.path.join(cur_dir, 'first_name.txt'))]
-        alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y', 'z']
-        nums = ['1','2','3','4','5','6','7','8','9','0']
-        self.stop_words = first_words + alphabets + nums
+        f_data = open("../data/new_all_data.json",'r')
+        self.f_out = open("illness.json","w")
+        self.all_data = json.load(f_data)
+        f_data.close()
+        self.all_illness = list()
+        first_words = open('first_name.txt').readlines()[0].split(' ')
+        alphabets = list("abcdefghijklmnopqrstuvwxyz0123456789")
+        self.stop_words = set(first_words + alphabets)
         self.key_dict = {
-            '医保疾病' : 'yibao_status',
-            "患病比例" : "get_prob",
+            '医保疾病' : 'medicare',
+            "患病比例" : "get_proportion",
             "易感人群" : "easy_get",
             "传染方式" : "get_way",
             "就诊科室" : "cure_department",
             "治疗方式" : "cure_way",
-            "治疗周期" : "cure_lasttime",
-            "治愈率" : "cured_prob",
+            "治疗周期" : "cure_time",
+            "治愈率" : "cured_probability",
             '药品明细': 'drug_detail',
             '药品推荐': 'recommand_drug',
             '推荐': 'recommand_eat',
             '忌食': 'not_eat',
-            '宜食': 'do_eat',
+            '宜食': 'good_eat',
             '症状': 'symptom',
             '检查': 'check',
             '成因': 'cause',
             '预防措施': 'prevent',
             '所属类别': 'category',
-            '简介': 'desc',
+            '简介': 'description',
             '名称': 'name',
             '常用药品' : 'common_drug',
-            '治疗费用': 'cost_money',
+            '治疗费用': "money",
             '并发症': 'acompany'
         }
         self.cuter = CutWords()
@@ -49,7 +45,8 @@ class MedicalGraph:
         cates = []
         inspects = []
         count = 0
-        for item in self.col.find():
+        for item in self.all_data:
+
             data = {}
             basic_info = item['basic_info']
             name = basic_info['name']
@@ -59,7 +56,7 @@ class MedicalGraph:
             data['名称'] = name
             data['简介'] = '\n'.join(basic_info['desc']).replace('\r\n\t', '').replace('\r\n\n\n','').replace(' ','').replace('\r\n','\n')
             category = basic_info['category']
-            data['所属类别'] = category
+            #data['所属类别'] = category
             cates += category
             inspect = item['inspect_info']
             inspects += inspect
@@ -70,19 +67,15 @@ class MedicalGraph:
             # 并发症
             data['症状'] = list(set([i for i in item["symptom_info"][0] if i[0] not in self.stop_words]))
             for attr in attributes:
-                attr_pair = attr.split('：')
+                attr_pair  = attr.split('：')
                 if len(attr_pair) == 2:
                     key = attr_pair[0]
                     value = attr_pair[1]
                     data[key] = value
             # 检查
             inspects = item['inspect_info']
-            jcs = []
-            for inspect in inspects:
-                jc_name = self.get_inspect(inspect)
-                if jc_name:
-                    jcs.append(jc_name)
-            data['检查'] = jcs
+
+            data['检查'] = inspects
             # 食物
             food_info = item['food_info']
             if food_info:
@@ -95,44 +88,39 @@ class MedicalGraph:
             data['药品明细'] = drug_info
             data_modify = {}
             for attr, value in data.items():
+                if attr[0]==' ':
+                    attr = attr[1:]
                 attr_en = self.key_dict.get(attr)
+                print(attr_en)
                 if attr_en:
                     data_modify[attr_en] = value
-                if attr_en in ['yibao_status', 'get_prob', 'easy_get', 'get_way', "cure_lasttime", "cured_prob"]:
+                if attr_en in ['medicare', 'get_proportion', 'easy_get', 'get_way', "cure_time", "cured_probability"]:
                     data_modify[attr_en] = value.replace(' ','').replace('\t','')
                 elif attr_en in ['cure_department', 'cure_way', 'common_drug']:
                     data_modify[attr_en] = [i for i in value.split(' ') if i]
+                
                 elif attr_en in ['acompany']:
                     acompany = [i for i in self.cuter.max_biward_cut(data_modify[attr_en]) if len(i) > 1]
                     data_modify[attr_en] = acompany
 
             try:
-                self.db['medical'].insert(data_modify)
+                #self.db['medical'].insert(data_modify)
                 count += 1
                 print(count)
+                #print(data_modify)
+                self.all_illness.append(data_modify)
             except Exception as e:
                 print(e)
+            
+        json.dump(self.all_illness, self.f_out)
+        self.f_out.close()
+
 
         return
-
-
-    def get_inspect(self, url):
-        res = self.db['jc'].find_one({'url':url})
-        if not res:
-            return ''
-        else:
-            return res['name']
-
-    def modify_jc(self):
-        for item in self.db['jc'].find():
-            url = item['url']
-            content = item['html']
-            selector = etree.HTML(content)
-            name = selector.xpath('//title/text()')[0].split('结果分析')[0]
-            desc = selector.xpath('//meta[@name="description"]/@content')[0].replace('\r\n\t','')
-            self.db['jc'].update({'url':url}, {'$set':{'name':name, 'desc':desc}})
 
 
 
 if __name__ == '__main__':
     handler = MedicalGraph()
+    handler.collect_medical()
+    
